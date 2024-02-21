69a81ae2505611c857ad661da0377ca75101b26f
Hugo Silva <hugofigueira.dev@gmail.com>
2024-02-21 17:08:41 +0000

    Adding string types

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exemplo_1 = "Isto representa uma das estrelas da constelação Vulpecula: \u2605 \n";
+            Console.WriteLine(exemplo_1);
+
+            string exemplo_2 = "Depois desta frase, vai ter uma nova linha.\nEu avisei!";
+            Console.WriteLine(exemplo_2);
+
+        }
+    }
+}
