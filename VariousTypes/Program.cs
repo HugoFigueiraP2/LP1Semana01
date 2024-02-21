f8a37c5cb8b401063ff3a6c48325cc7efcd96289
Hugo Silva <hugofigueira.dev@gmail.com>
2024-02-21 16:38:20 +0000

    Variaveis Inteiras

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+
+            byte numero = 23;
+            ushort numero_2 = 500;
+            uint numero_3 = 23506;
+            short numero_4 = -2;
+            Console.WriteLine($"{numero} {numero_2} {numero_3} {numero_4}"); 
+        }
+    }
+}
