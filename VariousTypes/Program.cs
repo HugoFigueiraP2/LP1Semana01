471828ea008a4dd4d2792f5a3c1f8bb5662a62da
Hugo Silva <hugofigueira.dev@gmail.com>
2024-02-21 16:50:29 +0000

    Adding char types

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -12,6 +12,12 @@ namespace VariousTypes
             uint numero_3 = 23506;
             short numero_4 = -2;
             Console.WriteLine($"{numero} {numero_2} {numero_3} {numero_4}"); 
+
+            char sinal_certo = '\u2713';
+            char estrela = '\u2605';
+            char euro = '\u20AC';
+
+            Console.WriteLine($"{sinal_certo} {estrela} {euro}");
         }
     }
 }
