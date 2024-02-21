5f7e8346936775b48229bff38799e344f3794687
Hugo Silva <hugofigueira.dev@gmail.com>
2024-02-21 17:25:07 +0000

    Adding other types of format in strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,17 +6,31 @@ namespace ManyStrings
     {
         static void Main(string[] args)
         {
+            double xx = 1.23456;
+            int ii = 19;
+            Console.WriteLine($"xx = {xx:f2} || xx = {xx:p1}");
+            Console.WriteLine($"ii = 0x{ii:x} || ii = {ii:c}");
+
+
+
+
+
+
+
+
+
+
             //string exemplo_1 = "Isto representa uma das estrelas da constelação Vulpecula: \u2605 \n";
             //Console.WriteLine(exemplo_1);
 
             //string exemplo_2 = "Depois desta frase, vai ter uma nova linha.\nEu avisei!";
             //Console.WriteLine(exemplo_2);
 
-            int x = 25;
-            string juntar_1 = "Adicionar strings com inteiros, tipo esta frase + " + 2 + ",nunca ia dar certo em Python!";
-            string juntar_2 = $"Imagina ter {x} gatos em casa... É o meu sonho!";
-            Console.WriteLine(juntar_1);
-            Console.WriteLine(juntar_2);
+            //int x = 25;
+            //string juntar_1 = "Adicionar strings com inteiros, tipo esta frase + " + 2 + ",nunca ia dar certo em Python!";
+            //string juntar_2 = $"Imagina ter {x} gatos em casa... É o meu sonho!";
+            //Console.WriteLine(juntar_1);
+            //Console.WriteLine(juntar_2);
 
         }
     }
