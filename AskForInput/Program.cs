9dd976355c7ba7369283f9706647cb227a1c65be
Hugo Silva <hugofigueira.dev@gmail.com>
2024-02-21 17:35:09 +0000

    Program that showes the result of a sum

---
+++ AskForInput/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adicione um número inteiro: ");
+            string User_1 = Console.ReadLine();
+
+            int numero_1 = int.Parse(User_1);
+
+            Console.WriteLine("Adicione um número real: ");
+            string User_2 = Console.ReadLine();
+
+            float numero_2 = float.Parse(User_2);
+
+            float soma = numero_1 + numero_2;
+            Console.WriteLine($"Resultado da soma: {soma}");
+
+        }
+    }
+}
