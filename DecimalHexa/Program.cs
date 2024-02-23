02bb6857ebeb1a20a606e9efbf296652b5be292d
JNetoGH <joaomendes15@gmail.com>
2024-02-23 16:24:14 +0000

    Adding variables

---
+++ DecimalHexa/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DecimalHexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insere 4 número inteiros:");
+            string user_1 = Console.ReadLine();
+            string user_2 = Console.ReadLine();
+            string user_3 = Console.ReadLine();
+            string user_4 = Console.ReadLine();
+
+            string hex_1 = $"{user_1:x}";
+            Console.WriteLine(hex_1);
+
+
+        }
+    }
+}
