114141ec8729b34d6000bcaacbf1afe6d2ca0489
JNetoGH <joaomendes15@gmail.com>
2024-02-23 16:34:54 +0000

    Converting strings to int to be able to convert to hex

--- DecimalHexa/Program.cs
+++ DecimalHexa/Program.cs
@@ -12,8 +12,20 @@ namespace DecimalHexa
             string user_3 = Console.ReadLine();
             string user_4 = Console.ReadLine();
 
-            string hex_1 = $"{user_1:x}";
-            Console.WriteLine(hex_1);
+            int dec_1 = int.Parse(user_1);
+            int dec_2 = int.Parse(user_2);
+            int dec_3 = int.Parse(user_3);
+            int dec_4 = int.Parse(user_4);
+
+
+            string hex_1 = $"{dec_1,6:x}";
+            string hex_2 = $"{dec_2,6:x}";
+            string hex_3 = $"{dec_3,6:x}";
+            string hex_4 = $"{dec_4,6:x}";
+            Console.WriteLine($"   {user_1}{hex_1}");
+            Console.WriteLine($"   {user_2}{hex_2}");
+            Console.WriteLine($"   {user_3}{hex_3}");
+            Console.WriteLine($"   {user_4}{hex_4}");
 
 
         }
